08902a77989023988bf326ea35fdc94678b3bfde
sunshine <tajohnsonn@gmail.com>
2020-10-29 15:18:09 -0700

    finished the html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+//the path package sends the file path to html page
+
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
